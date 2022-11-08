d136b2e8cbfd234687a8a6a1ed6cc4a87b125e6b
jguevarar1995 <jguevarar1@ucentral.edu.co>
2022-11-08 17:20:23 -0500

    add CORS protocol for security options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { SERVER_PORT } from './config/constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const port = +configService.get<number>(SERVER_PORT);
   await app.listen(port);
